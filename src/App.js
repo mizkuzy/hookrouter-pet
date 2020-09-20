c32bb4f84e8846630fac58816a05c9dc77eb5934
lyudmila.rudkova@kandasoft.com <MisteR2901>
2020-09-20 14:33:29 +0300

    Add redirect

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Navbar from "./components/NavBar";
-import { useRoutes } from "hookrouter";
+import { useRedirect, useRoutes } from "hookrouter";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
 const routes = {
-  "/": () => <Home />,
+  "/home": () => <Home />,
   "/about*": () => <About />,
-  "/contact/:uid": ({uid}) => <Contact uid={uid} />,
+  "/contact/:uid": ({ uid }) => <Contact uid={uid} />,
 };
 
 function App() {
+  useRedirect("/", "home");
   const match = useRoutes(routes);
 
   return (
